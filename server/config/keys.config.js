71b92a797ca29ef80cc722270238925fa3ba2aa5
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-10-28 11:41:27 -0600

    created connection string

--- server/config/keys.config.js
+++ server/config/keys.config.js
@@ -1,9 +1,9 @@
-const dbUsername = 'ybicwqpyafegxj';
-const dbPassword = '8e8ac57a5ac61e28024c5d9e7288eb6e5a9344190bb382af02ba2390ee28076c';
-const dbName = 'dcuhvugu57296a';
+const username = 'ybicwqpyafegxj';
+const password = '8e8ac57a5ac61e28024c5d9e7288eb6e5a9344190bb382af02ba2390ee28076c';
+const name = 'dcuhvugu57296a';
 
 module.exports = {
-    dbUsername,
-    dbPassword,
-    dbName,
+    username,
+    password,
+    name,
 };
