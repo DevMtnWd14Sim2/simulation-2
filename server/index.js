4d0e3b19afefef52fdb38e89996e0686586a1e34
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-10-28 11:27:05 -0600

    initialized middleware

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const chalk = require('chalk');
+const addMiddlewareTo = require('./middleware/decorate.middleware');
 
 const app = express();
+
+addMiddlewareTo(app);
 
 const port = process.env.PORT || 8080;
 app.listen(
