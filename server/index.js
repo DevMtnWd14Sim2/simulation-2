e0bbdb4dcadc1ba6250c2d1fb6b85798a2be2634
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-10-28 11:55:44 -0600

    created paths for routers

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const chalk = require('chalk');
 const addMiddlewareTo = require('./middleware/decorate.middleware');
+const delegateRoutesFor = require('./routers/delegate.router');
 
 const app = express();
 
 addMiddlewareTo(app);
+delegateRoutesFor(app);
 
 const port = process.env.PORT || 8080;
 app.listen(
---
+++ server/routers/delegate.router.js
@@ -0,0 +1,14 @@
+const authRouter = require('./auth.router');
+const dashboardRouter = require('./dashboard.router');
+const wizardRouter = require('./wizard.router');
+
+function delegate(app) {
+
+    app.use('/auth', authRouter);
+    app.use('/dashboard', dashboardRouter);
+    app.use('/wizard', wizardRouter);
+
+    return app;
+}
+
+module.exports = delegate;
