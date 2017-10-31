ec4b5023130819db07ddf75b42fc72c0be395ad8
landonwjohnson <landonwjohnson@gmail.com>
2017-10-30 18:36:33 -0600

    the app is more responsive

--- src/components/AuthView.js
+++ src/components/AuthView.js
@@ -6,12 +6,13 @@ export default class AuthView extends React.Component {
   render() {
     return (
       <div className="auth-view-container">
+        <div className="form-body-auth">
         <img className="auth-logo" src={AuthLogo} />
         <form>
-          <div className="auth-title-tag">Username</div>
+          <div className="form-label">Username</div>
               <input className="auth-type" type = "text" />
 
-          <div className="auth-title-tag">Password</div>
+          <div className="form-label">Password</div>
               <input className="auth-type" type = "password" />
 
           <div className="button-set">
@@ -20,6 +21,7 @@ export default class AuthView extends React.Component {
           </div>
           
         </form>
+        </div>
       </div>
     );
   }
