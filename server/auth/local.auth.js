ede7154d1934548a87e6232073b6f1e3aeafd3a0
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-02 17:58:25 -0600

    converted promises in local auth strategies to SQL instead of Mongo

--- server/auth/local.auth.js
+++ server/auth/local.auth.js
@@ -7,51 +7,57 @@ const getDb = require('../database/bootstrap.database');
 passport.use('login', new Strategy(
     (username, password, done) => {
         const db = getDb();
-        db.users.findOne( username, (err, user) => {
-            if (err) {
-                console.error(`Error in login: ${err}.`);
-                return done(err, false, {message: `Error in login: ${err}.`});
-            }
-            if (!user) {
-                console.error('That username does not exist.');
-                return done(null, false, {message: 'That username does not exist.'});
-            }
-            if (!user.verifyPassword(password)) {
-                console.error('That password is incorrect.');
-                return done(null, false, {message: 'That password is incorrect.'});
-            }
-            console.log(chalk.green('Login was successful!'))
-            return done(null, user, {message: 'Login was successful!'});
-        });
+        db.users
+            .findOne({username})
+            .then(user => {
+                if (!user) {
+                    console.error('That username does not exist');
+                    return done({message: 'That username does not exist.'});
+                }
+                if (!user.verifyPassword(password)) {
+                    console.error('That password is incorrect.');
+                    return done({message: 'That password is incorrect.'});
+                }
+                console.log(chalk.green('Login was successful!'));
+                return done(null, user, {message: 'Login was successful.'});
+            })
+            .catch(err => {
+                console.error('Error with login.');
+                return done(err, false, {message: 'Error with login.'});
+            });
     }
 ));
 
 passport.use('register', new Strategy(
     (username, password, done) => {
         const db = getDb();
-        db.users.findOne( username, (err, user) => {
-            if (err) {
-                console.error(`Error in registration: ${err}.`);
-                return done(err, false, {message: `Error in registration: ${err}.`});
-            }
-            if (user) {
-                console.error('That username already exists.');
-                return done(null, false, {message: 'That username already exists.'});
-            }
-            else {
-                const newUser = new User();
-                newUser.username = username;
-                newUser.password = createHash(password);
-                newUser.save(err => {
-                    if (err) {
-                        console.error(`Error in saving new user: ${err}.`);
-                        throw err;
-                    }
-                    console.log(chalk.green('Registration was successful!'));
-                    return done(null, newUser, {message: 'Registration was successful!'});
-                })
-            }
-        })
+        db.users
+            .findOne({username})
+            .then(user => {
+                if (user) {
+                    console.error('That username already exists.');
+                    return done({message: 'That username already exists.'});
+                }
+                else {
+                    const newUser = new User();
+                    newUser.username = username;
+                    newUser.password = createHash(password);
+                    db.users
+                        .save(newUser)
+                        .then( () => {
+                            console.log(chalk.green('Registration was successful!'));
+                            return done(null, newUser, {message: 'Registration was successful!'});
+                        })
+                        .catch(err => {
+                            console.error('Error in saving new user.');
+                            return done({message: 'Error in saving new user.'});
+                        });
+                }
+            })
+            .catch(err => {
+                console.error('Error with registration.');
+                return done({message: 'Error with registration.'});
+            });
     }
 ));
 
@@ -61,11 +67,13 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser((user, done) => {
     const db = getDb();
-    db.users.findOne(id, (err, user) => {
-        if (err) {
-            console.error(`Error in deserializing user: ${err}.`);
-            return done(err, false, {message: `Error in deserializing user: ${err}.`});
-        }
-        return done(null, user, {message: 'User has been deserialized.'});
-    });
+    db.users
+        .findOne({id})
+        .then(user => {
+            return done(null, user, {message: 'User has been deserialized'});
+        })
+        .catch(err => {
+            console.error('Error in deserializing user.');
+            return done({message: 'Error in deserializing user.'});
+        });
 });
