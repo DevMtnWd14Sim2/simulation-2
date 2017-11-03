1a8150b71f2520398da949989e4d4325be63fcf6
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-11-02 19:42:19 -0600

    getting ready to merge

--- src/components/AuthView.js
+++ src/components/AuthView.js
@@ -6,49 +6,7 @@ import { Link } from 'react-router-dom';
 import { authenticateUser,  registerUser } from '../services/auth.services';
 
 
-export default class AuthView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: 'james@gmail.com',
-      password: 'yak'
-    };
-    this.login = this.login.bind(this);
-    this.register = this.register.bind(this);
-  }
-
-  login() {
-    const userObject = {...this.state};
-    authenticateUser(userObject)
-      .then(res => {
-        console.log(res);
-        if (res.status === 200) {
-          this.props.history.push('/dashboard');
-        }
-        return res;
-      })
-      .catch(err => {
-        console.error('login was NOT successful.');
-        throw err;
-      });
-  }
-
-  register() {
-    const userObject = {...this.state};
-    registerUser(userObject)
-      .then(res => {
-        console.log(res);
-        if (res.status === 200) {
-          alert('Registration successful! Try logging in now.');
-        }
-        return res;
-      })
-      .catch(err => {
-        console.error('registration was NOT successful.');
-        throw err;
-      });
-  }
-
+export default class AuthView extends React.Component {
   render() {
     return (
       <div className="auth-view-container">
@@ -62,8 +20,8 @@ export default class AuthView extends Component {
               <input className="auth-type" type = "password" required/>
 
           <div className="button-set">
-            <span className="login-btn" onClick={this.login}>Login</span>
-            <span className="reg-btn" onClick={this.register}>Register</span>
+            <button className="login-btn">Login</button>
+            <button className="reg-btn">Register</button>
           </div>
           
         </form>
